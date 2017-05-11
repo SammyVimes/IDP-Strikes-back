c4fac93c9d3d534ca20badcd7fa8f083b70d51d7
Леонид Скороспелов < >
2017-05-11 13:43:01 +0300

    Added working forms of pills and food

---
+++ addpilldialog.cpp
@@ -0,0 +1,19 @@
+#include "addpilldialog.h"
+
+AddPillDialog::AddPillDialog(QWidget *parent) : QDialog(parent)
+{
+    setupUi(this);
+    this->pillAfterRadioButton->setChecked(true);
+    this->pillBeforeRadioButton->setChecked(false);
+    this->pillNameLineEdit->clear();
+    this->amountLineEdit->clear();
+}
+
+void AddPillDialog::on_addPillDialog_accepted()
+{
+    int mask = 0;
+    mask |= (this->pillBreakfastCheckBox->isChecked() << 2);
+    mask |= (this->pillLunchCheckBox->isChecked() << 1);
+    mask |= (this->pillDinnerCheckBox->isChecked());
+    emit readyToAddPill(Pill(this->pillNameLineEdit->text(), this->pillLifeTimeSpinBox->value(), this->pillBeforeRadioButton->isChecked(), mask, this->amountLineEdit->text()));
+}
